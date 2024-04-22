7694aa57bccaa83a65a2c78a41b19af583997b43
felixmakinda <felixmakinda@gmail.com>
2024-04-22 21:15:08 +0300

    fix error in mongdb Url

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {};
 // Update to have your correct username and password
 config.mongoURI = {
   production:
-    'mongodb+srv://felixmakinda:@ILoveJesus1994@darkroom.rr98p0m.mongodb.net/?retryWrites=true&w=majority&appName=darkroom"',
+    "mongodb+srv://felixmakinda:@ILoveJesus1994@darkroom.rr98p0m.mongodb.net/?retryWrites=true&w=majority&appName=darkroom",
   development: "mongodb://localhost:27017/",
   test: "mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority",
 };
