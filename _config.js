fa0747dda35616256129dad2bb49714eb0b9f333
felixmakinda <felixmakinda@gmail.com>
2024-04-22 21:10:43 +0300

    update mongoDB url in test config.js

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production:
+    'mongodb+srv://felixmakinda:@ILoveJesus1994@darkroom.rr98p0m.mongodb.net/?retryWrites=true&w=majority&appName=darkroom"',
+  development: "mongodb://localhost:27017/",
+  test: "mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority",
+};
 module.exports = config;
